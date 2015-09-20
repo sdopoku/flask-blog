45ef37085ac695dc61c9852ed7aa6afd2d5a27ad
David S Opoku <david@adoorelabs.com>
2015-09-20 07:06:11 +0000

    Require user login to access main page.

--- blog.py
+++ blog.py
@@ -3,6 +3,7 @@
 # imports
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 # configuration
 DATABASE = 'blog.db'
@@ -19,6 +20,16 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE_PATH']
     )
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -38,6 +49,7 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
