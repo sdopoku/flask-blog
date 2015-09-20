55c71307a91637f5b952f8f0bd3144c9b503806e
David S Opoku <david@adoorelabs.com>
2015-09-20 06:50:58 +0000

    Add login and logout features.

---
+++ blog.py
@@ -0,0 +1,45 @@
+# blog.py - controller
+
+# imports
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
+
+# configuration
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\xbc\xb4\x17\xdf\xe6\x05hS\xe3\xf5\xc0]P$4]4\xdd\xd5\xeaU\xba-\xa0'
+
+app = Flask(__name__)
+
+# pull in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+# function used for connecting to the database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE_PATH']
+    )
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
